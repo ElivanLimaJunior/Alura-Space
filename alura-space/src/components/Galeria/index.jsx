dd9fcaec300e4bf92281f885e349bd6aeb7ba348
ElivanLimaJunior <85586631+ElivanLimaJunior@users.noreply.github.com>
2022-11-02 16:18:39 -0300

    Refatorando Código dos Cards
    Criamos um componente só com as informações dos cards.

--- alura-space/src/components/Galeria/index.jsx
+++ alura-space/src/components/Galeria/index.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import Tags from "../tags";
-import styles from "./Galeria.module.scss";
+import style from "./Galeria.module.scss";
 import fotos from "./fotos.json";
 import Cards from "./cards";
 
 export default function Galeria() {
   return (
-    <section className={styles.galeria}>
-      <h2>Navegue pela galeria</h2>
-      <Tags />
-      <Cards itens={fotos} styles={styles} />
+    <section className={style.galeria}>
+      <div className={style.galeriaImg}>
+        <h2>Navegue pela galeria</h2>
+        <Tags />
+        <Cards itens={fotos} style={style} />
+      </div>
     </section>
   );
 }
